e5e148f426b26b45c9b4cee9c96de7c9951d3382
kmiyazaki3 <tktrnoknlw@gmail.com>
2019-04-24 14:40:58 +0900

    first comment

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/test/test_data/test_python/test_django/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
